252dd57b29eb013e63b2a666035adf1c0f029f0a
qixiaotian <15148192179@163.com>
2019-08-19 14:25:50 +0800

    设置登录页面背景图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-   home
+  <div class="login">
+
   </div>
 </template>
 
@@ -9,3 +9,12 @@ export default {
 
 }
 </script>
+<style lang="less" scoped>
+   .login{
+      width: 100%;
+      height: 100vh;
+      background-image: url('../assets/img/login_bg.jpg');
+      background-color:red;
+      background-size : cover;
+   }
+</style>
