a8fa89cf6fe45246e591a1336ea1c1323f388a59
qixiaotian <15148192179@163.com>
2019-08-20 08:49:39 +0800

    新建首页

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <!-- <router-link to="/"></router-link>| -->
+      <router-link to="/"></router-link>|
       <!-- <router-link to="/about">About</router-link> -->
-      <router-view></router-view>
     </div>
     <router-view></router-view>
   </div>
