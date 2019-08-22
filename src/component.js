0674a355540dee6bd85fc71287c6fd726795decc
qixiaotian <15148192179@163.com>
2019-08-22 14:06:02 +0800

    面包屑组件安装

--- src/component.js
+++ src/component.js
@@ -1,8 +1,11 @@
 import layAside from './components/home/layout-aside'
 import layHeader from './components/home/layout-header.vue'
+// 注册面包屑组件
+import breadCrumb from './components/common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('lay-aside', layAside)
     Vue.component('lay-header', layHeader)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }
