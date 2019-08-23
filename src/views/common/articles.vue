cda371a15931eac176b46ff5ab3d94b6269527b5
qixiaotian <15148192179@163.com>
2019-08-23 20:04:14 +0800

    内容列表-页面结构

--- src/views/common/articles.vue
+++ src/views/common/articles.vue
@@ -14,14 +14,14 @@
         </el-radio-group>
       </el-form-item>
       <el-form-item label="频道列表">
-          <el-select v-model="formData" placeholder="请选择">
-            <el-option
-              v-for="item in options"
-              :label="item.label"
-              :value="item.value"
-              :key="item.value"
-            ></el-option>
-          </el-select>
+        <el-select v-model="formData" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :label="item.label"
+            :value="item.value"
+            :key="item.value"
+          ></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item label="时间选择">
         <el-date-picker
@@ -32,21 +32,27 @@
         ></el-date-picker>
       </el-form-item>
     </el-form>
-
-    <el-row type="flex" justify="space-between">
-      <el-col :span="10">
-        <img src alt />
-        <el-col>
-          <span>你说</span>
-          <el-tag>已发表</el-tag>
-          <span>2019-1-23</span>
-        </el-col>
-      </el-col>
-      <el-col :span="4">
-        <el-button>编辑</el-button>
-        <el-button>删除</el-button>
-      </el-col>
-    </el-row>
+    <div class="total_title">共找到56789条符合条件的内容</div>
+    <div class="content-list">
+      <div class="content-item" v-for="(item,index) in list" :key="index">
+        <div class="left">
+          <img src="../../assets/img/404.png" alt />
+          <div class="info">
+            <span>我是内容标题</span>
+            <el-tag type="width:60px">标签一</el-tag>
+            <span class="data">2019-08-22 17:21:31</span>
+          </div>
+        </div>
+        <div class="right">
+          <span>
+            <i class="el-icon-edit"></i>修改
+          </span>
+          <span>
+            <i class="el-icon-delete"></i>删除
+          </span>
+        </div>
+      </div>
+    </div>
   </el-card>
 </template>
 
@@ -78,15 +84,54 @@ export default {
           value: '选项5',
           label: '北京烤鸭'
         }
-      ]
+      ],
+      list: [1, 2, 3]
     }
   }
 }
 </script>
-
-<style>
-  .formDate {
-    margin-left: 40px;
-    margin-top: 40px;
+<style lang="less" scoped>
+  .total_title {
+    height: 60px;
+    line-height: 60px;
+    border-bottom: 1px dashed #ccc;
+  }
+  .content-list {
+    .content-item {
+      padding: 10px;
+      border-bottom: 1px solid #f2f3f5;
+      display: flex;
+      justify-content: space-between;
+      .left {
+        display: flex;
+        align-items: center;
+        img {
+          width: 150px;
+          height: 100px;
+          border-radius: 4px;
+        }
+        .info {
+          margin-left: 10px;
+          display: flex;
+          height: 100px;
+          padding: 5px 0;
+          flex-direction: column;
+          justify-content: space-between;
+          .data{
+              color:#999;
+              font-size: 12px;
+          }
+        }
+      }
+      .right {
+          span,span i {
+              font-size: 12px;
+              color: #333;
+          }
+          span{
+              margin-right: 5px
+          }
+      }
+    }
   }
 </style>
