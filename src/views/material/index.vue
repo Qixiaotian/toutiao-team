70922a94720cabf9e727783503bf0457a36aa685
qixiaotian <15148192179@163.com>
2019-08-23 10:07:11 +0800

    图片的收藏和取消

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,50 +1,53 @@
 <template>
-  <el-card >
-   <bread-crumb>
-      <template slot="tilte">素材管理
-      </template>
-   </bread-crumb>
-   <el-tabs v-model="activeName" @tab-click="changeTab">
-     <el-tab-pane label="全部图片" name="all">
- <div class="img-list">
-   <el-card class="img-card" v-for="item in list" :key="item.id">
-     <img :src="item.url" alt="">
-      <el-row align="middle" class="operate" type="flex" justify="space-around">
-            <i class="el-icon-star-off"></i>
-            <i class="el-icon-delete-solid"></i>
-      </el-row>
- </el-card>
+  <el-card>
+    <bread-crumb>
+      <template slot="tilte">素材管理</template>
+    </bread-crumb>
+    <el-tabs v-model="activeName" @tab-click="changeTab">
+      <el-tab-pane label="全部图片" name="all">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+            <el-row align="middle" class="operate" type="flex" justify="space-around">
+              <i
+                class="el-icon-star-off"
+                @click="collectFun(item)"
+                :style="{color:item.is_collected?'red':''}"></i>
+
+              <i class="el-icon-delete-solid" @click="delMaterial(item)"></i>
+            </el-row>
+          </el-card>
         </div>
-<el-row type='flex' justify="center">
-            <el-pagination
-              :current-page="page.currentPage"
-              :page-size="page.pageSize"
-              :total="page.total"
-              @current-change="changePage"
-              background
-              layout="prev, pager, next"
-            ></el-pagination>
-          </el-row>
-    </el-tab-pane>
+        <el-row type="flex" justify="center">
+          <el-pagination
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            :total="page.total"
+            @current-change="changePage"
+            background
+            layout="prev, pager, next"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
       <el-tab-pane label="收藏图片" name="collect">
         <div class="img-list">
           <el-card class="img-card" v-for="item in list" :key="item.id">
             <img :src="item.url" alt />
           </el-card>
         </div>
-         <el-row type='flex' justify="center">
-            <el-pagination
-              :current-page="page.currentPage"
-              :page-size="page.pageSize"
-              :total="page.total"
-              @current-change="changePage"
-              background
-              layout="prev, pager, next"
-            ></el-pagination>
-          </el-row>
+        <el-row type="flex" justify="center">
+          <el-pagination
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            :total="page.total"
+            @current-change="changePage"
+            background
+            layout="prev, pager, next"
+          ></el-pagination>
+        </el-row>
       </el-tab-pane>
     </el-tabs>
-    </el-card>
+  </el-card>
 </template>
 <script>
 export default {
@@ -56,10 +59,34 @@ export default {
         currentPage: 1,
         pageSize: 15,
         total: 0
-      } }
+      }
+    }
   },
   methods: {
+    collectFun (item) {
+      let mess = item.is_colleted ? '取消收藏' : '收藏'
+      this.$confirm('您是否是' + mess + '收藏图片?', '提示').then(() => {
+        this.$axios({
+          url: `user/images/${item.id}`,
+          method: 'put',
+          data: { collect: !item.is_colleted }
+        }).then(result => {
+          this.getMaterial()
+        })
+      })
+    },
     // 切换页码
+    // 删除素材的方法
+    delMaterial (item) {
+      this.$confirm('您确定删除这个图片吗', '提示').then(() => {
+        this.$axios({
+          method: 'delete',
+          url: `user/images/${item.id}`
+        }).then(result => {
+          this.getMaterial()
+        })
+      })
+    },
     changePage (newPage) {
       this.page.currentPage = newPage
       this.getMaterial()
@@ -85,34 +112,35 @@ export default {
   },
   created () {
     this.getMaterial() // 第一次进入 activeName === "all"  加载第一页的全部数据
-  } }
+  }
+}
 </script>
 
 <style lang="less" scoped>
-.img-list {
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-  .img-card {
-    width: 200px;
-    height: 200px;
-    margin: 20px 10px;
-    position: relative;
-    img {
-      width: 100%;
-      height: 100%;
-    }
-    .operate {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      height: 30px;
-      width: 100%;
-      background-color: #f4f5f6;
-      i {
-        font-size: 22px;
+  .img-list {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    .img-card {
+      width: 200px;
+      height: 200px;
+      margin: 20px 10px;
+      position: relative;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+      .operate {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        height: 30px;
+        width: 100%;
+        background-color: #f4f5f6;
+        i {
+          font-size: 22px;
+        }
       }
     }
   }
-}
 </style>
